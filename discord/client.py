6be813ec2694ac43f2c24aa4817a045c2030488d
Timothe Duquesne <timothe.duquesne@gmail.com>
2025-12-13 15:47:49 +0100

    update: added more debug for discord

--- discord/client.py
+++ discord/client.py
@@ -14,8 +14,18 @@ def send_embed(webhook_url: str | None, payload: DiscordPayload):
     try:
         # Send the payload to the webhook
         response = requests.post(url=webhook_url, json=payload_clean, timeout=5)
+        
+        # Debug in case of failure
+        if response.status_code == 400:
+            print("🚨 DISCORD 400 BAD REQUEST DETECTED")
+            print(f"👉 Payload sent (partial): {str(payload_clean)[:500]}...")
+            print(f"👉 DISCORD RESPONSE: {response.text}")
+
         # Raise an error if their is a problem
         response.raise_for_status()
         print("📨 Message sent to Discord !")
     except requests.exceptions.RequestException as e:
         print(f"❌ Error while sending to Webhook : {e}")
+
+        if hasattr(e, 'response') and e.response is not None:
+            print(f"🔍 Server Response content: {e.response.text}")
